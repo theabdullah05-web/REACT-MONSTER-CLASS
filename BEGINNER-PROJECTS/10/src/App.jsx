d1614f7303e61d02f61415cbfd7ddb81816560dd
Abdulah Hussain <abdullahbhtty01@gmail.com>
2026-08-23 19:55:43 +0500

    added new project

---
+++ BEGINNER-PROJECTS/10/src/App.jsx
@@ -0,0 +1,15 @@
+import Nav from "./Navigation/Nav";
+import Products from "./Products/Products";
+import Recommended from "./Recommended/Recommended";
+import "./index.css";
+const App = () => {
+  return (
+    <>
+      <Nav />
+      <Products />
+      <Recommended />
+    </>
+  );
+};
+
+export default App;
