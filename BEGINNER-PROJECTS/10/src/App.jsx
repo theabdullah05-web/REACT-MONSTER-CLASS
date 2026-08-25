a6e594060197f63937dc41d4231ffb78a8395fe5
Abdulah Hussain <abdullahbhtty01@gmail.com>
2026-08-25 13:40:28 +0500

    commit

--- BEGINNER-PROJECTS/10/src/App.jsx
+++ BEGINNER-PROJECTS/10/src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import Nav from "./Navigation/Nav";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
 import SideBar from "./Sidebar/Sidebar";
 import "./index.css";
 const App = () => {
+  const [selectedCategory, useSelectedCategory] = useState(null);
+  //Input Filter
+  const [query, setQuery] = useState("");
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
   return (
     <>
       <SideBar />
