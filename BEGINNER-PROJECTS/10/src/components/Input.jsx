0b735b20ab349d31cc7341a37825fa532ebf58ef
Abdullah <abdullahbhtty01@gmail.com>
2026-08-25 15:19:10 +0500

    Auto commit 2026-08-25 15:19:09

--- BEGINNER-PROJECTS/10/src/components/Input.jsx
+++ BEGINNER-PROJECTS/10/src/components/Input.jsx
@@ -1,8 +1,9 @@
 export default function Input({ handleChange, value, title, name, color }) {
   return (
     <label className="sidebar-label-container">
-      <input type="radio" name="test2" />
-      <span className="check-mark"></span>All
+      <input type="radio" onChange={handleChange} value={value} name={name} />
+      <span className="check-mark" style={{ backgroundColor: color }}></span>
+      {title}
     </label>
   );
 }
