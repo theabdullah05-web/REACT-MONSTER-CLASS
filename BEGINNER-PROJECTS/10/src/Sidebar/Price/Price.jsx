7a3847edb931bd0fdca6ccebbfe544a251f027b0
Abdullah <abdullahbhtty01@gmail.com>
2026-08-25 16:09:03 +0500

    Auto commit 2026-08-25 16:09:03

--- BEGINNER-PROJECTS/10/src/Sidebar/Price/Price.jsx
+++ BEGINNER-PROJECTS/10/src/Sidebar/Price/Price.jsx
@@ -15,6 +15,24 @@ const Price = ({ handleChange }) => {
         title="$0-50"
         name="test2"
       />
+      <Input
+        handleChange={handleChange}
+        value={100}
+        title="$50-100"
+        name="test2"
+      />
+      <Input
+        handleChange={handleChange}
+        value={150}
+        title="$100-150"
+        name="test2"
+      />
+      <Input
+        handleChange={handleChange}
+        value={200}
+        title="Over $150"
+        name="test2"
+      />
     </div>
   );
 };
