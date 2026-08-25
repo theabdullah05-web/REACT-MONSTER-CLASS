ec04831c8aeafded8a7dcdf6e2de8ea6e72ad348
Abdullah <abdullahbhtty01@gmail.com>
2026-08-25 15:22:17 +0500

    Auto commit 2026-08-25 15:22:17

--- BEGINNER-PROJECTS/10/src/Sidebar/Category/Category.jsx
+++ BEGINNER-PROJECTS/10/src/Sidebar/Category/Category.jsx
@@ -1,12 +1,12 @@
 import Input from "../../components/Input";
 import "./Category.css";
-const Category = () => {
+const Category = ({ handleChange }) => {
   return (
     <div>
       <h2 className="sidebar-title">Category</h2>
       <div>
         <label className="sidebar-label-container">
-          <input />
+          <input onChange={handleChange} type="radio" value={test} />
         </label>
       </div>
     </div>
