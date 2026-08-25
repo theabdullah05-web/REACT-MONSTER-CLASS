8f2488d7bd7096119a376ee7972f0d06f9bfb6db
Abdullah <abdullahbhtty01@gmail.com>
2026-08-25 16:04:10 +0500

    Auto commit 2026-08-25 16:04:09

--- BEGINNER-PROJECTS/10/src/Sidebar/Price/Price.jsx
+++ BEGINNER-PROJECTS/10/src/Sidebar/Price/Price.jsx
@@ -1,7 +1,7 @@
 import Input from "../../components/Input";
 import "./Price.css";
 
-const Price = () => {
+const Price = ({ handleChange }) => {
   return (
     <div className="ml">
       <h2 className="sidebar-title price-title">Price</h2>
