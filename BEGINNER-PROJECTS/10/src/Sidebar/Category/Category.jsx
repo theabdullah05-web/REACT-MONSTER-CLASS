79bef04171efda0b162dbba245327472cdcaa977
Abdullah <abdullahbhtty01@gmail.com>
2026-08-25 15:20:57 +0500

    Auto commit 2026-08-25 15:20:57

--- BEGINNER-PROJECTS/10/src/Sidebar/Category/Category.jsx
+++ BEGINNER-PROJECTS/10/src/Sidebar/Category/Category.jsx
@@ -5,7 +5,9 @@ const Category = () => {
     <div>
       <h2 className="sidebar-title">Category</h2>
       <div>
-        <Input />
+        <label className="sidebar-label-container">
+          <Input />
+        </label>
       </div>
     </div>
   );
