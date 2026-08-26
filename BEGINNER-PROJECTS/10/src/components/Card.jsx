d097ffe9b213a165e51b4e6a0f50fd96e709d6a6
Abdullah <abdullahbhtty01@gmail.com>
2026-08-26 15:45:09 +0500

    Auto commit 2026-08-26 15:45:09

--- BEGINNER-PROJECTS/10/src/components/Card.jsx
+++ BEGINNER-PROJECTS/10/src/components/Card.jsx
@@ -8,6 +8,9 @@ const Card = ({ img, title, star, reviews, newPrice, prevPrice }) => {
         <h3 className="card-title">{title}</h3>
         <section className="card-reviews">
           {star}
+          {star}
+          {star}
+          {star}
           <span className="total-reviews">{reviews}</span>
         </section>
         <section className="card-price">
