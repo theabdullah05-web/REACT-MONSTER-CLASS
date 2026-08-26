046a9de373d6c9abc8c2ae4ba4c982514cdb2e2a
Abdullah <abdullahbhtty01@gmail.com>
2026-08-26 15:53:46 +0500

    Auto commit 2026-08-26 15:53:45

--- BEGINNER-PROJECTS/10/src/Recommended/Recommended.jsx
+++ BEGINNER-PROJECTS/10/src/Recommended/Recommended.jsx
@@ -6,7 +6,11 @@ const Recommended = () => {
       <h2 className="recommended-title">Recommended</h2>
       <div className="recommended-flex">
         <button className="btns">All Products</button>
-        <Buttons />
+        <Buttons onClickHandler={handleClick} value="Nike" title={"Nike"} />
+        <Buttons onClickHandler={handleClick} value="Adidas" title={"Adidas"} />
+        <Buttons onClickHandler={handleClick} value="Adidas" title={"Adidas"} />
+        <Buttons onClickHandler={handleClick} value="Puma" title={"Puma"} />
+        <Buttons onClickHandler={handleClick} value="Vans" title={"Vans"} />
       </div>
     </div>
   );
