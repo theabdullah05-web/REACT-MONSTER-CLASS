5d9608f689997cd9dc2153aa38a651cfbf956b66
Abdullah <abdullahbhtty01@gmail.com>
2026-08-26 15:33:42 +0500

    Auto commit 2026-08-26 15:33:42

--- BEGINNER-PROJECTS/10/src/Sidebar/Colors/Colors.jsx
+++ BEGINNER-PROJECTS/10/src/Sidebar/Colors/Colors.jsx
@@ -44,6 +44,7 @@ const Colors = ({ handleChange }) => {
           name="test1"
         />
         <span
+          className="ceckmark"
           style={{ backgroundColor: "white", border: "1px solid black" }}
         ></span>
       </label>
