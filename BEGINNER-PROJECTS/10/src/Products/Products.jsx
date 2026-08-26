0cba2de7ba678861fca8486d1d8769d2524c4508
Abdullah <abdullahbhtty01@gmail.com>
2026-08-26 15:39:11 +0500

    Auto commit 2026-08-26 15:39:11

--- BEGINNER-PROJECTS/10/src/Products/Products.jsx
+++ BEGINNER-PROJECTS/10/src/Products/Products.jsx
@@ -1,11 +1,9 @@
 import Card from "../components/Card";
 import "./Products.css";
-const Products = () => {
+const Products = ({ result }) => {
   return (
     <>
-      <section className="card-container">
-        <Card />
-      </section>
+      <section className="card-container">{result}</section>
     </>
   );
 };
