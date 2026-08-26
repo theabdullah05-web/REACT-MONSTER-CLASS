62ebe742d338f86158b5f06fafcfadea29e12abe
Abdullah <abdullahbhtty01@gmail.com>
2026-08-26 15:51:27 +0500

    Auto commit 2026-08-26 15:51:27

--- BEGINNER-PROJECTS/10/src/Recommended/Recommended.jsx
+++ BEGINNER-PROJECTS/10/src/Recommended/Recommended.jsx
@@ -6,10 +6,7 @@ const Recommended = () => {
       <h2 className="recommended-title">Recommended</h2>
       <div className="recommended-flex">
         <button className="btns">All Products</button>
-        <button className="btns">Nike</button>
-        <button className="btns">Puma</button>
-        <button className="btns">Adidas</button>
-        <button className="btns">Vans</button>
+        <Buttons />
       </div>
     </div>
   );
