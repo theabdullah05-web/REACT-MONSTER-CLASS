fe490b7c77d6c92f961b1fd5a250a56bc7fe0725
Abdullah <abdullahbhtty01@gmail.com>
2026-09-05 15:57:26 +0500

    commit

--- REACT with TypeScript/1. Component Props Typing/src/App.tsx
+++ REACT with TypeScript/1. Component Props Typing/src/App.tsx
@@ -1,10 +1,13 @@
-import User from "./components/User";
+// import User from "./components/User";
+
+import Button from "./components/Button";
 
 export default function App() {
   // return <User name="Abdullah" age={21} isStudent={true} />;
-  return (
-    <User>
-      <p>Abdullah is here</p>
-    </User>
-  );
+  // return (
+  //   <User>
+  //     <p>Abdullah is here</p>
+  //   </User>
+  // );
+  return <Button />;
 }
