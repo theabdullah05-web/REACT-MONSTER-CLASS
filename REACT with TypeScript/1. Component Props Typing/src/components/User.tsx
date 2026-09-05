ae9932f3e38e41f904c108ccd73e606fc9d11f40
Abdullah <abdullahbhtty01@gmail.com>
2026-09-05 15:54:26 +0500

    commit

--- REACT with TypeScript/1. Component Props Typing/src/components/User.tsx
+++ REACT with TypeScript/1. Component Props Typing/src/components/User.tsx
@@ -3,19 +3,29 @@
 //   age: number;
 //   isStudent: boolean;
 // };
-interface userInterface {
-  name: string;
-  age: number;
-  isStudent: boolean;
+// interface userInterface {
+//   name: string;
+//   age: number;
+//   isStudent: boolean;
+// }
+// const User = ({ name, age, isStudent }: userInterface) => {
+//   return (
+//     <div>
+//       <h1>{name}</h1>
+//       <h2>{age}</h2>
+//       <h3>{isStudent}</h3>
+//     </div>
+//   );
+
+import type { ReactNode } from "react";
+
+// };
+interface childrenShape {
+  children: ReactNode;
 }
-const User = ({ name, age, isStudent }: userInterface) => {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <h2>{age}</h2>
-      <h3>{isStudent}</h3>
-    </div>
-  );
+
+export const User = ({ children }: childrenShape) => {
+  return { children };
 };
 
 export default User;
