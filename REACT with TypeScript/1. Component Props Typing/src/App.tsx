f17e976dc88a88b87e141dc906e4b9bcbcb5778d
Abdullah <abdullahbhtty01@gmail.com>
2026-09-05 15:55:35 +0500

    commit

--- REACT with TypeScript/1. Component Props Typing/src/App.tsx
+++ REACT with TypeScript/1. Component Props Typing/src/App.tsx
@@ -4,7 +4,7 @@ export default function App() {
   // return <User name="Abdullah" age={21} isStudent={true} />;
   return (
     <User>
-      <p>Hi,Abdullah is here</p>
+      <p>Abdullah is here</p>
     </User>
   );
 }
