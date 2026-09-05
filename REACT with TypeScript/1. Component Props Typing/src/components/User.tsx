501ecb4e8b0f9157efdfbd32e4db6ab4462c2f7e
Abdullah <abdullahbhtty01@gmail.com>
2026-09-05 15:46:42 +0500

    commit

--- REACT with TypeScript/1. Component Props Typing/src/components/User.tsx
+++ REACT with TypeScript/1. Component Props Typing/src/components/User.tsx
@@ -1,9 +1,14 @@
-type userShape = {
+// type userShape = {
+//   name: string;
+//   age: number;
+//   isStudent: boolean;
+// };
+interface userInterface {
   name: string;
   age: number;
   isStudent: boolean;
-};
-const User = ({ name, age, isStudent }: userShape) => {
+}
+const User = ({ name, age, isStudent }: userInterface) => {
   return (
     <div>
       <h1>{name}</h1>
