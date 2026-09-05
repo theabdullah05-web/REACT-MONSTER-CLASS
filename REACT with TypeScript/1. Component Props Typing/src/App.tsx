8b61cbd943448301f6ba7427d9024e23653c21a0
Abdullah <abdullahbhtty01@gmail.com>
2026-09-05 16:05:10 +0500

    commit

--- REACT with TypeScript/1. Component Props Typing/src/App.tsx
+++ REACT with TypeScript/1. Component Props Typing/src/App.tsx
@@ -9,5 +9,14 @@ export default function App() {
   //     <p>Abdullah is here</p>
   //   </User>
   // );
-  return <Button />;
+  let onclick = () => {
+    alert("Button was clicked");
+  };
+  let nothing = () => {};
+  return (
+    <>
+      <Button label="Click Me" disabled={false} onClick={onclick} />
+      <Button label="Disabled" disabled={true} onClick={nothing} />
+    </>
+  );
 }
