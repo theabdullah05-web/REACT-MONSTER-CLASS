e9db3b1b656419e571dd323d5b910c38c4c4a2b7
Abdullah <abdullahbhtty01@gmail.com>
2026-09-05 16:15:26 +0500

    commit

--- REACT with TypeScript/1. Component Props Typing/src/App.tsx
+++ REACT with TypeScript/1. Component Props Typing/src/App.tsx
@@ -25,7 +25,7 @@ export default function App() {
   // );
   return (
     <UserInfo
-      id={Math.floor(Math.random() * 100)}
+      id={Math.floor(Math.random() * 1000000)}
       name="John"
       email="john@abc"
     />
