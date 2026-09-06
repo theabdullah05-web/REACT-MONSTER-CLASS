58eef8c2b852b04c5137da05ba0e5a73a5b126ff
Abdullah <abdullahbhtty01@gmail.com>
2026-09-06 22:42:06 +0500

    commit

--- REACT with TypeScript/UseState Types/src/App.tsx
+++ REACT with TypeScript/UseState Types/src/App.tsx
@@ -1,10 +1,16 @@
 // import Counter from "./components/Counter";
 
 import { useState } from "react";
-import UserProfile from "./components/userProfile";
+import UserProfile from "./components/UserProfile";
+import TodoList from "./components/TodoList";
 
 export const App = () => {
   const [state, setState] = useState<object>({});
   // return <Counter />;
-  return <UserProfile information={state} func={setState} />;
+  return (
+    <>
+      <UserProfile information={state} func={setState} />
+      <TodoList/>
+    </>
+  );
 };
---
+++ REACT with TypeScript/UseState Types/src/components/UserProfile.tsx
@@ -0,0 +1,9 @@
+const userProfile = ({information, func}:{information:object,func:()=>void}) => {
+  return (
+    <div>
+      {}
+    </div>
+  )
+}
+
+export default userProfile
