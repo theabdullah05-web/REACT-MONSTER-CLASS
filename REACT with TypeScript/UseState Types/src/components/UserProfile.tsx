e7deecc54b280bd70da8cd53b6b3d48a48fd6b48
Abdullah <abdullahbhtty01@gmail.com>
2026-09-07 16:08:47 +0500

    commit

--- REACT with TypeScript/UseState Types/src/components/UserProfile.tsx
+++ REACT with TypeScript/UseState Types/src/components/UserProfile.tsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 const UserProfile = () => {
-  return <h2>UserProfile</h2>;
+  interface profile {
+    name: string;
+    age: number;
+    email: string;
+  }
+  const [profile, setProfile] = useState<profile>({
+    name: "Abdullah",
+    age: 21,
+    email: "something@gmail.com",
+  });
+  return (
+    <>
+      <h3>Profile Info</h3>
+      <p>Name:{profile.name}</p>
+      <p>Age:{profile.age}</p>
+      <p>Email:{profile.email}</p>
+    </>
+  );
 };
 
 export default UserProfile;
