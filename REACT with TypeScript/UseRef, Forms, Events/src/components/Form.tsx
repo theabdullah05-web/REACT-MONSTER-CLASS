2aef38489ef4e8036236f753b123d99dee15f75e
Abdullah <abdullahbhtty01@gmail.com>
2026-09-07 17:17:25 +0500

    commit

--- REACT with TypeScript/UseRef, Forms, Events/src/components/Form.tsx
+++ REACT with TypeScript/UseRef, Forms, Events/src/components/Form.tsx
@@ -1,4 +1,16 @@
+import { useState } from "react";
+
 const Form = () => {
+  type formData = {
+    name: string;
+    email: string;
+    password: string;
+  };
+  const [form, setForm] = useState<formData>({
+    name: "",
+    email: "",
+    password: "",
+  });
   return <form></form>;
 };
 
