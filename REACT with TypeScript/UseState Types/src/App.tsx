11df45ae19172066f02e437b77d948e8f279131c
Abdullah <abdullahbhtty01@gmail.com>
2026-09-07 16:39:27 +0500

    commit

--- REACT with TypeScript/UseState Types/src/App.tsx
+++ REACT with TypeScript/UseState Types/src/App.tsx
@@ -1,12 +1,15 @@
 import Counter from "./components/Counter";
 import UserProfile from "./components/UserProfile";
 import "./App.css";
+import TodoList from "./components/TodoList";
 export const App = () => {
   return (
     <>
       <Counter />
       <br></br>
       <UserProfile />
+      <br></br>
+      <TodoList />
     </>
   );
 };
