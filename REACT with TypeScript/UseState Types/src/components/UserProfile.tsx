fec1ba2e7ff7fdf3b2903e3e5efbd4aa8a2296b1
Abdullah <abdullahbhtty01@gmail.com>
2026-09-07 16:20:22 +0500

    commit

--- REACT with TypeScript/UseState Types/src/components/UserProfile.tsx
+++ REACT with TypeScript/UseState Types/src/components/UserProfile.tsx
@@ -44,7 +44,7 @@ const UserProfile = () => {
       />
       <br></br>
       <h3>Profile Info</h3>
-      <h4>Name: {profile.name}</h4>
+      {profile.name !== "" && <h4>Name: {profile.name}</h4>}
       <h4>Age: {profile.age}</h4>
       <h4>Email: {profile.email}</h4>
     </>
