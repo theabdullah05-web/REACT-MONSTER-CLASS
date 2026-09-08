b7ab1b8f8ebcb33ce79cc33966a3221dce1cffac
Abdullah <abdullahbhtty01@gmail.com>
2026-09-08 14:30:33 +0500

    commit

--- REACT with TypeScript/UseRef, Forms, Events/src/components/ContactForm.tsx
+++ REACT with TypeScript/UseRef, Forms, Events/src/components/ContactForm.tsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+interface form {
+  name: string;
+  email: string;
+}
 const ContactForm = () => {
+  const [formData, setFormData] = useState<form>({
+    name: "",
+    email: "",
+  });
   return <div>ContactForm</div>;
 };
 
