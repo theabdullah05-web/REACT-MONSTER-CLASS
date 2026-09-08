af6c154b527584129682f9c5ceec6fc27ec8b455
Abdullah <abdullahbhtty01@gmail.com>
2026-09-08 15:17:05 +0500

    commit

--- REACT with TypeScript/Context API/src/components/useContext.tsx
+++ REACT with TypeScript/Context API/src/components/useContext.tsx
@@ -1,4 +1,17 @@
+import { createContext } from "react";
+
+interface context {
+  count: number;
+  increment: () => void;
+  decrement: () => void;
+}
+
 const useContext = () => {
+  const MyContext = createContext<context>({
+    count: 0,
+    increment: () => {},
+    decrement: () => {},
+  });
   return <></>;
 };
 
