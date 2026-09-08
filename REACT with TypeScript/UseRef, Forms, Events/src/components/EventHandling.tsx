794164f5c83a080ea9ac067edbf136ad7b427afc
Abdullah <abdullahbhtty01@gmail.com>
2026-09-08 14:58:48 +0500

    commit

--- REACT with TypeScript/UseRef, Forms, Events/src/components/EventHandling.tsx
+++ REACT with TypeScript/UseRef, Forms, Events/src/components/EventHandling.tsx
@@ -1,7 +1,12 @@
+import type { MouseEvent } from "react";
+
 const EventHandling = () => {
+  const handleMouseEnter = (e: MouseEvent<HTMLDivElement>) => {
+    console.log("Mouse Entered:", e.currentTarget);
+  };
   return (
     <>
-      <div>
+      <div onMouseEnter={handleMouseEnter}>
         <h1>Event Handling</h1>
         <h2>Exercise</h2>
       </div>
