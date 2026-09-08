0f9d76b2cb7df793cd3317b4b98401f9be56a47c
Abdullah <abdullahbhtty01@gmail.com>
2026-09-08 14:56:22 +0500

    commit

--- REACT with TypeScript/UseRef, Forms, Events/src/components/EventHandling.tsx
+++ REACT with TypeScript/UseRef, Forms, Events/src/components/EventHandling.tsx
@@ -1,5 +1,12 @@
 const EventHandling = () => {
-  return <div>EventHandling</div>;
+  return (
+    <>
+      <div>
+        <h1>Event Handling</h1>
+        <h2>Exercise</h2>
+      </div>
+    </>
+  );
 };
 
 export default EventHandling;
