03ce42469b07c6cdab7fa89b5931ff03e8357936
Abdullah <abdullahbhtty01@gmail.com>
2026-09-08 15:34:59 +0500

    commit

--- REACT with TypeScript/Context API/src/components/Counter.tsx
+++ REACT with TypeScript/Context API/src/components/Counter.tsx
@@ -1,5 +1,15 @@
+import { useContext } from "react";
+import { MyContext } from "./MyContext";
+
 const Counter = () => {
-  return <div>Counter</div>;
+  const { count, increment, decrement } = useContext(MyContext);
+  return (
+    <>
+      <h2>{count}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </>
+  );
 };
 
 export default Counter;
