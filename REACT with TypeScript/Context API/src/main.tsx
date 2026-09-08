080e0b1d2342752f5cb994a27956b17ac8d18370
Abdullah <abdullahbhtty01@gmail.com>
2026-09-08 15:36:20 +0500

    commit

--- REACT with TypeScript/Context API/src/main.tsx
+++ REACT with TypeScript/Context API/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
-import MyProvider from "./components/useContext.tsx";
+import MyProvider from "./components/MyProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
