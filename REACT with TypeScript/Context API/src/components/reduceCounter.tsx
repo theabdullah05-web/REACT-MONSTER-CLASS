f6b7dd32a9c46eb9be796a8cf54f0a5d92ea729e
Abdullah <abdullahbhtty01@gmail.com>
2026-09-08 16:03:34 +0500

    commit

--- REACT with TypeScript/Context API/src/components/reduceCounter.tsx
+++ REACT with TypeScript/Context API/src/components/reduceCounter.tsx
@@ -1,6 +1,22 @@
 import { useReducer } from "react";
 
+type state = {
+  count: number;
+};
+
+type action = { type: "Increment" } | { type: "Decrement" };
+
 const reduceCounter = () => {
+  const reducer = (state: state, action: action): state => {
+    switch (action.type) {
+      case "Increment":
+        return { count: state.count + 1 };
+      case "Decrement":
+        return { count: state.count - 1 };
+      default:
+        return state;
+    }
+  };
   const [state, dispatch] = useReducer(reducer, { count: 0 });
   return (
     <>
