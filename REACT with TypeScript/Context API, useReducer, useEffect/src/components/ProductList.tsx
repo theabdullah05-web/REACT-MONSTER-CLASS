4502cb83457591f34c32c9d3349b0148b9fe6982
Abdullah <abdullahbhtty01@gmail.com>
2026-09-09 18:16:39 +0500

    commit

---
+++ REACT with TypeScript/Context API, useReducer, useEffect/src/components/ProductList.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+type Product = {
+  id: number;
+  name: string;
+  price: number;
+};
+const ProductList = () => {
+  const [state, setState] = useState<Product[]>([
+    { id: 1, name: "Abdullah", price: 2005 },
+    { id: 2, name: "Mubasher", price: 1977 },
+    { id: 3, name: "Hussain", price: 2024 },
+  ]);
+  return (
+    <ul>
+      {state.map((el) => (
+        <li key={el.id}>
+          {el.name}
+          {el.price}
+          <button></button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ProductList;
