903d2b651068d532ceb50b399d3eb737ae6fbb0f
Abdullah <abdullahbhtty01@gmail.com>
2026-09-09 17:30:59 +0500

    commit

--- REACT with TypeScript/Context API, useReducer, useEffect/src/components/UserList.tsx
+++ REACT with TypeScript/Context API, useReducer, useEffect/src/components/UserList.tsx
@@ -13,18 +13,7 @@ const UserList = () => {
     const fetchData = async () => {
       const res = await fetch("https://jsonplaceholder.typicode.com/users");
       const results = await res.json();
-      for (result of results) {
-        setData((preVal) => [
-          ...preVal,
-          {
-            id: result.id,
-            name: result.name",
-            username: result.username,
-            email: result.email,
-            phone: result.phone,
-          },
-        ]);
-      }
+      setData(results);
     };
     fetchData();
   }, []);
