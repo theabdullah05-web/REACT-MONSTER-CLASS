d28a9fe44437a3d568d09c5bff9db21890a05382
Abdullah <abdullahbhtty01@gmail.com>
2026-09-09 16:28:56 +0500

    commit

--- REACT with TypeScript/Context API, useReducer, useEffect/src/components/MyComponent.tsx
+++ REACT with TypeScript/Context API, useReducer, useEffect/src/components/MyComponent.tsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react";
+
 const MyComponent = () => {
-  return <div>MyComponent</div>;
+  type data = {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    stock: number;
+    brand: string;
+    category: string;
+    thumbnail: string;
+    images: string[];
+  };
+  const [data, setData] = useState<data | null>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("https://dummyjson.com/product/1");
+        const parsedData = res.json();
+        setData(parsedData);
+      } catch (error) {
+        console.error("Fetching data error:", error);
+      }
+    };
+    fetchData();
+  }, []);
+  return <div>{data ? <></> : <></>}</div>;
 };
 
 export default MyComponent;
