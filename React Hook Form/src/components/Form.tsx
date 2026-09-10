c55c36240f4f04364aa3a3322ffe9705870551ac
Abdullah <abdullahbhtty01@gmail.com>
2026-09-10 17:44:38 +0500

    commit

--- React Hook Form/src/components/Form.tsx
+++ React Hook Form/src/components/Form.tsx
@@ -1,4 +1,4 @@
-import { useForm } from "react-hook-form";
+import { useForm, type SubmitHandler } from "react-hook-form";
 
 const Form = () => {
   interface formData {
@@ -11,7 +11,8 @@ const Form = () => {
     handleSubmit,
     formState: { errors, isSubmitting },
   } = useForm<formData>();
-  return <div>Form</div>;
+  const onSubmit: SubmitHandler<formData> = (data) => console.log(data);
+  return <form onSubmit={handleSubmit(onSubmit)}></form>;
 };
 
 export default Form;
