2ca31cbdd34f692052ecf56d9aed1d9fdf6c0ab5
Abdullah <abdullahbhtty01@gmail.com>
2026-09-10 18:28:57 +0500

    commit

--- React Hook Form/src/components/AdvancedForm.tsx
+++ React Hook Form/src/components/AdvancedForm.tsx
@@ -1,6 +1,12 @@
 import "../style.css";
 const AdvancedForm = () => {
-  return <div className="form-container"></div>;
+  return (
+    <div className="form-container">
+      <form>
+        <div></div>
+      </form>
+    </div>
+  );
 };
 
 export default AdvancedForm;
