ee919fc2eca947e60835f052fe47b6eeb8171820
Abdullah <abdullahbhtty01@gmail.com>
2026-09-10 17:38:41 +0500

    commit

--- React Hook Form/src/components/Form.tsx
+++ React Hook Form/src/components/Form.tsx
@@ -1,4 +1,12 @@
+import { useForm } from "react-hook-form";
+
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm();
   return <div>Form</div>;
 };
 
